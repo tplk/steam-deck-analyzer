055d5525b6bc3ac9acd17718229b027fa3901775
Dmitriy Teplov <dm@teplov.xyz>
2023-12-17 10:42:43 +0400

    feat: parse apps in known directories, print directory sizes

--- steam_deck_analyzer.lua
+++ steam_deck_analyzer.lua
@@ -29,7 +29,9 @@ local json = require 'deps.json'
 -- 620 Portal 2
 
 --- @alias Database { name: string, url: string }
---- @alias Config { cache_path: string, database: { update_interval: number, steam: Database } }
+--- @alias Config { cache_path: string, database: { update_interval: number, steam: Database, custom: { local_name: string } } }
+--- @alias AppId string
+--- @alias AppName string
 
 --- @type Config
 local config = {
@@ -39,6 +41,9 @@ local config = {
     steam = {
       name = 'steam',
       url = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/',
+    },
+    custom = {
+      local_name = 'custom_database'
     }
   },
 }
@@ -59,6 +64,12 @@ local logger = {
     print('Error: ' .. message)
   end,
 }
+
+--- @param str string
+--- @return string
+local function trim(str)
+  return str:match("^%s*(.-)%s*$")
+end
 
 --- @param path string
 --- @return boolean
@@ -84,6 +95,21 @@ local function is_directory(path)
   end
 end
 
+--- @param str string
+--- @param len number
+--- @param char string?
+local function pad_start(str, len, char)
+  if char == nil then char = ' ' end
+  return string.rep(char, len - #str) .. str
+end
+
+--- @param str string
+--- @param len number
+--- @param char string?
+local function pad_end(str, len, char)
+  if char == nil then char = ' ' end
+  return str .. string.rep(char, len - #str)
+end
 
 --- @return Error?
 local function init_file_system()
@@ -157,7 +183,15 @@ local function update_steam_database(database)
       goto continue
     end
 
-    database_file:write(app.appid .. ' ' .. app.name .. '\n')
+    local app_id = trim(tostring(app.appid))
+    local app_name = trim(app.name)
+
+    if app_id == '' or app_name == '' then
+      logger.debug('Invalid app data\n' .. json.encode(app))
+      goto continue
+    end
+
+    database_file:write(app_id .. ' ' .. app_name .. '\n')
 
     ::continue::
   end
@@ -165,6 +199,219 @@ local function update_steam_database(database)
   database_file:close()
 
   return nil
+end
+
+--- @return string[]?, Error?
+local function get_data_locations()
+  -- known locations are steamapp directories,
+  -- they contain compatdata, shadercache and downloading directories.
+  --
+  -- known locations:
+  -- $HOME/.steam/steam/steamapps
+  -- /run/media/DEVICE/steamapps
+  --
+  -- $HOME should be read from env
+  -- DEVICE count is unknown. find it by checking available. ignore "deck" device.
+
+  local known_locations = {
+    'shadercache',
+    -- TODO: I believe apps in common are already named?
+    -- 'common',
+    'compatdata',
+    'downloading',
+  }
+
+  --- @type string[]
+  local data_locations = {}
+
+  local steam_home = os.getenv('HOME') .. '/.steam/steam/steamapps'
+
+  for _, location in ipairs(known_locations) do
+    local location_path = steam_home .. '/' .. location
+
+    if is_directory(location_path) then
+      table.insert(data_locations, location_path)
+    end
+  end
+
+  local media_path = '/run/media'
+
+  if not is_directory(media_path) then
+    return data_locations
+  end
+
+  local ls_result = io.popen('ls ' .. media_path)
+
+  if ls_result == nil then
+    logger.error('Failed to list mounted devices in ' .. media_path)
+  else
+    local external_device_names = {}
+
+    for device_name in ls_result:lines() do
+      if device_name ~= 'deck' then
+        table.insert(external_device_names, device_name)
+      end
+    end
+
+    if #external_device_names > 0 then
+      for _, device_name in ipairs(external_device_names) do
+        local steamapps_path = media_path .. '/' .. device_name .. '/steamapps'
+
+        if is_directory(steamapps_path) then
+          for _, location in ipairs(known_locations) do
+            local location_path = steamapps_path .. '/' .. location
+
+            if is_directory(location_path) then
+              table.insert(data_locations, location_path)
+            end
+          end
+        end
+      end
+    end
+
+    ls_result:close()
+  end
+
+  return data_locations
+end
+
+-- TODO: search database faster, without iterating over the whole file?
+--- @alias Entry { app_id: AppId, app_name: AppName }
+--- @param app_ids AppId[]
+--- @param database Database
+--- @return Entry[]?, Error?
+local function find_apps_in_database(app_ids, database)
+  if #app_ids == 0 then
+    return {}
+  end
+
+  --- @type Entry[]
+  local apps = {}
+
+  --- @type table<AppId, AppId>
+  local lookup_app_id_table = {}
+
+  -- create lookup table for faster checking.
+  -- if app_id is 10 characters long it's not a Steam App, we can set it to unknown.
+  -- TODO: search for non steam apps, Steam shortcuts might have them.
+  -- It's also a good idea to save them because shortcut will be removed after
+  -- removing app from Steam.
+  -- refer to https://github.com/Matoking/protontricks/blob/master/src/protontricks/steam.py#L1159
+  for _, app_id in ipairs(app_ids) do
+    if #app_id == 10 then
+      table.insert(apps, { app_id = app_id, app_name = "Non-Steam App" })
+    else
+      lookup_app_id_table[app_id] = app_id
+    end
+  end
+
+  -- search custom database first, it's way smaller.
+  local custom_local_database_path = script_path() .. '/' .. config.database.custom.local_name
+  local custom_local_database_file = io.open(custom_local_database_path, 'r')
+
+  if custom_local_database_file == nil then
+    logger.warn('Failed to open custom database at ' .. custom_local_database_path)
+  else
+    for line in custom_local_database_file:lines() do
+      if not next(lookup_app_id_table) then
+        break
+      end
+
+      local app_id, app_name = line:match('(%d+) (.+)')
+
+      if app_id == nil or app_name == nil then
+        logger.debug('Invalid database entry: "' .. (line or "nil") .. '"')
+      elseif lookup_app_id_table[app_id] then
+        table.insert(apps, { app_id = app_id, app_name = app_name })
+        lookup_app_id_table[app_id] = nil
+      end
+    end
+
+    custom_local_database_file:close()
+  end
+
+  if not next(lookup_app_id_table) then
+    logger.debug('Found all apps in database, exit early.')
+    return apps
+  end
+
+  local database_path = config.cache_path .. '/' .. database.name
+  local database_file = io.open(database_path, 'r')
+
+  if database_file == nil then
+    return nil, 'Failed to open file ' .. database_path
+  end
+
+  -- skip first line since it's update timestamp.
+  _ = database_file:read('*line')
+
+  for line in database_file:lines() do
+    if not next(lookup_app_id_table) then
+      break
+    end
+
+    local app_id, app_name = line:match('(%d+) (.+)')
+
+    if app_id == nil or app_name == nil then
+      logger.debug('Invalid database entry: "' .. (line or "nil") .. '"')
+    elseif lookup_app_id_table[app_id] then
+      table.insert(apps, { app_id = app_id, app_name = app_name })
+      lookup_app_id_table[app_id] = nil
+    end
+  end
+
+  database_file:close()
+
+  if not next(lookup_app_id_table) then
+    logger.debug('Found all apps in database, exit early.')
+    return apps
+  end
+
+  -- fill up rest of the values with UNKNOWN
+  for app_id in pairs(lookup_app_id_table) do
+    table.insert(apps, { app_id = app_id, app_name = 'UNKNOWN' })
+  end
+
+  return apps
+end
+
+
+--- @param path string
+--- @return string?, Error?
+local function get_directory_size(path)
+  local du_result = io.popen('du -sh ' .. path)
+
+  if du_result == nil then
+    return nil, 'Failed to get directory size for ' .. path
+  end
+
+  local size = du_result:read('*line'):match('^(.+)\t')
+  du_result:close()
+
+  return size
+end
+
+--- @param path string
+--- @return AppId[]?, Error?
+local function list_apps_in_path(path)
+  local app_ids = {}
+
+  local ls_result = io.popen('ls ' .. path)
+
+  if ls_result == nil then
+    return nil, 'Failed to list apps in ' .. path
+  end
+
+  for app_id in ls_result:lines() do
+    -- validate if app name consists of numbers only
+    if app_id ~= " " and app_id:match('^%d+$') then
+      table.insert(app_ids, app_id)
+    end
+  end
+
+  ls_result:close()
+
+  return app_ids
 end
 
 
@@ -186,6 +433,54 @@ local function main()
     end
   else
     logger.debug('Database up to date.')
+  end
+
+  local data_locations
+  data_locations, err = get_data_locations()
+  if err then
+    logger.error(err)
+    os.exit(1)
+  end
+
+  if data_locations == nil then
+    logger.error('Error: Failed to find data locations.')
+    os.exit(1)
+  end
+
+  if #data_locations == 0 then
+    logger.log('No data locations found.')
+    os.exit(0)
+  end
+
+
+  for _, location in ipairs(data_locations) do
+    local location_app_ids
+    location_app_ids, err = list_apps_in_path(location)
+
+    if err then
+      logger.error(err)
+      os.exit(1)
+    end
+
+    if location_app_ids ~= nil and #location_app_ids > 0 then
+      local apps
+      apps, err = find_apps_in_database(location_app_ids, config.database.steam)
+
+      if err then
+        logger.error(err)
+        os.exit(1)
+      end
+
+      if apps ~= nil and next(apps) then
+        logger.log('---\nFound ' .. #apps .. ' apps in ' .. location)
+        for _, entry in pairs(apps) do
+          local size = get_directory_size(location .. '/' .. entry.app_id)
+          logger.log(pad_end((size or '??'), 5) .. ' ' .. pad_end(entry.app_id, 11) .. ' ' .. entry.app_name)
+        end
+      end
+    else
+      logger.log('---\nNo apps found in ' .. location)
+    end
   end
 end
 
